0187ad1ea371f3d9ebd199fca74ba488ca85fdef
Arman Hussan <Arman@MacBook-Air.local>
2025-08-23 01:20:45 +0600

    practics

--- Fibonacci_with_memoization.cpp
+++ Fibonacci_with_memoization.cpp
@@ -1,27 +1,27 @@
-//Fibonacci with Memoization:
+// Fibonacci with Memoization:
 #include <bits/stdc++.h>
 using namespace std;
 
 int fib(int n, int F[])
 {
-    if(n <= 1)
+    if (n <= 1)
     {
         F[n] = n;
         return F[n];
     }
 
-    if(F[n] == -1)
+    if (F[n] == -1)
     {
         F[n] = fib(n - 1, F) + fib(n - 2, F);
     }
 
     return F[n];
 }
- 
 
-
-int main() {
-    int n; cin >> n;
+int main()
+{
+    int n;
+    cin >> n;
     int F[n + 1];
     memset(F, -1, sizeof(F));
 
@@ -29,6 +29,5 @@ int main() {
 
     cout << result << endl;
 
-
-  return 0;
- }
+    return 0;
+}
