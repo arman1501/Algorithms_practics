a6802ae40afb4239233d8b6131af7c3fbc5f39a7
Arman Hussan <Arman@MacBook-Air.local>
2025-08-25 23:48:43 +0600

    ptactics

--- Articlution_point.cpp
+++ Articlution_point.cpp
@@ -8,57 +8,54 @@ int timer = 0;
 int n, e;
 
 // Void type function calling.
-void DFS (int u, int parent)
+void DFS(int u, int parent)
 {
     visit[u] = 1;
     dis[u] = low[u] = ++timer;
 
     int child = 0;
-    for(int v = 1; v <= n; v++)
+    for (int v = 1; v <= n; v++)
     {
-        if(visit[v] == 0 && graph[u][v] != 0)
+        if (visit[v] == 0 && graph[u][v] != 0)
         {
             child++;
-            DFS (v, u);
+            DFS(v, u);
 
             low[u] = min(low[u], low[v]);
             isAP[u] = true;
         }
-        else if(v != parent && graph[u][v] != 0)
+        else if (v != parent && graph[u][v] != 0)
         {
             low[u] = min(low[u], dis[v]);
         }
     }
 
-    if(parent == -1 && child > 1)
+    if (parent == -1 && child > 1)
     {
         isAP[u] = true;
     }
 }
 
-
-
- int main() {
+int main()
+{
     cin >> n >> e;
     int u, v;
-    for(int i = 1; i <= e; i++)
+    for (int i = 1; i <= e; i++)
     {
         cin >> u >> v;
         graph[u][v] = 1;
         graph[v][u] = 1;
     }
 
-    DFS(1, -1);     // Define void function called.
+    DFS(1, -1); // Define void function called.
 
-
-    for(int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if(isAP[i] == true)
+        if (isAP[i] == true)
         {
             cout << i << " is an AP" << endl;
         }
     }
-    
 
-  return 0;
- }
+    return 0;
+}
